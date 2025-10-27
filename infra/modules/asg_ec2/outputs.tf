e4de58a5a7fa54322365b2fda020b9acfc779ccc
hoooooomkke <hoooooomkke@gmail.com>
2025-10-27 14:18:29 +0900

    fix(asg_ec2): export app_sg_id (and keep asg_name)

--- infra/modules/asg_ec2/outputs.tf
+++ infra/modules/asg_ec2/outputs.tf
@@ -4,3 +4,8 @@ output "asg_name" {
   description = "AutoScalingGroup name (for CW alarms)"
   value       = aws_autoscaling_group.this.name
 }
+
+output "app_sg_id" {
+  description = "App EC2 Security Group ID (referenced by RDS SG)"
+  value       = aws_security_group.app_sg.id
+}
